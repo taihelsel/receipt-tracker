b1a6d741cb928342a676e62191c3a52229373229
taihelsel <tai.bayard@gmail.com>
2018-03-26 15:43:27 -0700

    Installed node files, and rendering home page

---
+++ index.js
@@ -0,0 +1,23 @@
+//Dependencies
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const ejsLayouts = require('express-ejs-layouts');
+const request = require("request");
+const session = require('express-session');
+const app = express();
+
+//setting view
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public'));
+//Controllers
+app.get('/', (req, res) => {
+    res.render("layout");
+});
+//Exports
+const server = app.listen(process.env.PORT || 3000);
+module.exports = server;
